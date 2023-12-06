17e82f97db21824dd6c96c09903bc5aa0c612e6a
Ostap4i <137522973+Ostap4i@users.noreply.github.com>
2023-12-06 19:16:28 +0200

    Add files via upload

---
+++ lesson 6/Man.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+    internal class Man
+    {
+        internal object mansName;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Man(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+
+
+      
+
+
+            
+            
+            
+
+
+
+    }
+}
---
+++ lesson 6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Man> mans = new List<Man>();
+            mans.Add(new Man("Ostap", 14));
+            mans.Add(new Man("Artur", 24));
+            mans.Add(new Man("Maks", 23));
+            mans.Add(new Man("Bogdan", 30));
+
+            mans.ForEach((man) =>
+            {
+                if (man.Age <= 25)
+                {
+                    Console.WriteLine(man.Name);
+                }
+
+            });
+            mans.RemoveAll((man) => man.Age <= 18);
+
+
+            Console.WriteLine("\nBefore sort:");
+            foreach (Man aMan in mans)
+            {
+                Console.WriteLine(aMan.Name);
+            }
+
+            mans.Sort();
+
+            Console.WriteLine("\nAfter sort by part number:");
+            foreach (Man aMan in mans)
+            {
+                Console.WriteLine(aMan.Name);
+            }
+
+
+            mans.Sort( (Man x, Man y) =>
+               x.Name.CompareTo(y.Name)
+            );
+
+            Console.WriteLine("\nAfter sort by name:");
+            foreach (Man aMan in mans)
+            {
+                Console.WriteLine(aMan.Name);
+            }
+
+      
+
+
+
+        }
+    }
+}
+
